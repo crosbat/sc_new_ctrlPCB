f36b23f8356fc1e7ace4c50b62d5a68918559db2
Abhijit Kulkarni <eku@energy.aau.dk>
2023-07-05 08:57:56 +0200

    adc update with a test comment

--- modules/sf-meas/measHandler.c
+++ modules/sf-meas/measHandler.c
@@ -88,7 +88,7 @@ __attribute__((weak)) bool measHandler_performMeas(void)
   gMeas.value = adcValue0MicroVolt;
 
 
-  ////////
+  //////// previous implementation by SF
 
   /* Provide dummy measurement*/
 //  gMeas.value = random_rand();
